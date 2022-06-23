f033ec0752d89fa3fbfc036a8af9e5ef113c86cf
Sam451363 <83856360+Sam451363@users.noreply.github.com>
2022-06-23 11:52:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+img = "";
+
+function preload() {
+    img = loadImage('dog_cat.jpg');
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Casper", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("Copper", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
